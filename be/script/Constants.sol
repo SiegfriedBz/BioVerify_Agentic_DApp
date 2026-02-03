4fff8855ab3bf500b47c8d4b34050e4f19aaee27
siegfriedbz <siegfried.bozza@yahoo.com>
2026-02-03 19:33:27 +0100

    chore(protocol): install chainlink-brownie-contracts and update constants

--- be/script/Constants.sol
+++ be/script/Constants.sol
@@ -3,5 +3,6 @@ pragma solidity ^0.8.13;
 
 contract Constants {
     uint256 constant SUBMISSION_FEE = 1 wei;
-    uint256 constant MIN_STAKE = 10 wei;
+    uint256 constant PUBLISHER_MIN_STAKE = 20 wei;
+    uint256 constant REVIEWER_MIN_STAKE = 10 wei;
 }
