0f2e05b0d311b69a73251bec798cd0f38a6b12fc
siegfriedbz <siegfried.bozza@yahoo.com>
2026-04-26 15:02:54 +0200

    feat(fe): improve publications realtime sync, stats invalidation, docs

--- apps/fe/app/(routes)/publications/layout.tsx
+++ apps/fe/app/(routes)/publications/layout.tsx
@@ -1,6 +1,7 @@
+import { SideProvider } from "@/_context/side-provider"
 import { headers } from "next/headers"
 import { Toaster } from "sonner"
-import { SideProvider } from "@/_context/side-provider"
+import { PublicationsRealtimeProvider } from "./_components/publications-realtime-provider"
 
 export default async function Layout({
 	children,
@@ -14,11 +15,13 @@ export default async function Layout({
 
 	return (
 		<SideProvider cookies={cookies}>
-			{breadcrumbs}
-			<main className="max-w-7xl flex flex-col gap-6 pt-6 pb-2">
-				{children}
-				<Toaster />
-			</main>
+			<PublicationsRealtimeProvider>
+				{breadcrumbs}
+				<main className="max-w-7xl flex flex-col gap-6 pt-6 pb-2">
+					{children}
+					<Toaster />
+				</main>
+			</PublicationsRealtimeProvider>
 		</SideProvider>
 	)
 }
